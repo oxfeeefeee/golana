63e78370707eed5a055eddb9c5d2f48bee70a61f
oxfeeefeee <pb01005051@gmail.com>
2023-03-30 23:04:29 +0800

    new example

---
+++ examples/helloworld/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt2"
+	. "solana"
+)
+
+// This is the definition of the IxInit Instruction
+type IxInit struct {
+	// First, list all the accounts that are used by the instruction
+	// Use tags to specify the account attributes:
+	// - `golana:"signer"` for the accounts that are used as signer
+	// - `golana:"mut"` for the accounts that are used as writable
+	user        *AccountInfo `golana:"signer"`
+	userAccount *AccountInfo `golana:"mut"`
+
+	// Second, declare the data stored in the accounts, that needs to be read or written by the instruction
+	// Use the corresponding account name with a _data suffix,
+	// and add the `golana:"init"` or `golana:"mut"` tag to the field:
+	// - `golana:"init"` for the data that will be initialized by the instruction
+	// - `golana:"mut"` for the data that will be written by the instruction
+	userAccount_data *UserData `golana:"init"`
+
+	// Finally, list all the instruction parameters
+	initialCount uint64
+}
+
+type UserData struct {
+	auth       PublicKey
+	greetCount uint64
+}
+
+// This is the business logic of the IxInit
+func (ix *IxInit) Process() {
+	data := new(UserData)
+	// set the auth of userAccount as the user
+	data.auth = *ix.user.Key
+	data.greetCount = ix.initialCount
+	ix.userAccount_data = data
+	CommitData(ix.userAccount)
+}
+
+type IxGreet struct {
+	user        *AccountInfo `golana:"signer"`
+	userAccount *AccountInfo `golana:"mut"`
+
+	userAccount_data *UserData `golana:"mut"`
+
+	name string
+}
+
+func (ix *IxGreet) Process() {
+	// Check that the user is the auth of the userAccount
+	assert(*ix.user.Key == ix.userAccount_data.auth)
+
+	fmt2.Println("Hello", ix.name, "you have been greeted", ix.userAccount_data.greetCount, "times")
+
+	// Increment the greetCount
+	ix.userAccount_data.greetCount++
+	CommitData(ix.userAccount)
+
+}
+
+// This is the entry point of the program
+func main() {
+	GetIx().Process()
+}
---
+++ examples/helloworld/tests/helloworld.ts
@@ -0,0 +1,93 @@
+import { IDL, Helloworld } from '../target/helloworld_idl.js';
+import { Program, initFromEnv } from "golana";
+import { ComputeBudgetProgram, Keypair, SystemProgram, Transaction } from '@solana/web3.js';
+import BN from 'bn.js';
+
+describe("helloworld", async () => {
+    try {
+        let provider = initFromEnv();
+
+        const hello = new Program<Helloworld>(IDL, await Program.createByteCodePubKey("helloworld"));
+
+        const userAccountSpace = 512;
+        const userAccount = Keypair.generate();
+        const payer = Keypair.generate();
+
+        it("Initialize program state", async () => {
+            // Airdropping tokens to a payer.
+            await provider.connection.confirmTransaction(
+                await provider.connection.requestAirdrop(payer.publicKey, 1000000000),
+                "processed"
+            );
+
+            const userAccountLamports = await provider.connection.getMinimumBalanceForRentExemption(userAccountSpace);
+
+            // Create the user account
+            await provider.sendAndConfirm(
+                (() => {
+                    const tx = new Transaction();
+                    tx.add(
+                        SystemProgram.createAccount({
+                            fromPubkey: payer.publicKey,
+                            newAccountPubkey: userAccount.publicKey,
+                            lamports: userAccountLamports,
+                            space: userAccountSpace,
+                            programId: hello.golanaLoader.programId,
+                        })
+                    );
+                    return tx;
+                })(),
+                [payer, userAccount],
+                { skipPreflight: true },
+            );
+
+        });
+
+        it("IxInit", async () => {
+            await hello.methods.IxInit(
+                new BN(666),
+            )
+                .accounts({
+                    user: payer.publicKey,
+                    userAccount: userAccount.publicKey,
+                })
+                .preInstructions([
+                    ComputeBudgetProgram.requestHeapFrame({ bytes: 256 * 1024 }),
+                    ComputeBudgetProgram.setComputeUnitLimit({ units: 1400000 })
+                ])
+                .signers([payer])
+                .rpc({ skipPreflight: true });
+        });
+
+        it("IxGreet", async () => {
+            await hello.methods.IxGreet("best_chain_devs")
+                .accounts({
+                    user: payer.publicKey,
+                    userAccount: userAccount.publicKey,
+                })
+                .preInstructions([
+                    ComputeBudgetProgram.requestHeapFrame({ bytes: 256 * 1024 }),
+                    ComputeBudgetProgram.setComputeUnitLimit({ units: 1400000 })
+                ])
+                .signers([payer])
+                .rpc({ skipPreflight: true });
+        });
+
+        it("IxGreet2", async () => {
+            await hello.methods.IxGreet("best_chain_devs")
+                .accounts({
+                    user: payer.publicKey,
+                    userAccount: userAccount.publicKey,
+                })
+                .preInstructions([
+                    ComputeBudgetProgram.requestHeapFrame({ bytes: 256 * 1024 }),
+                    ComputeBudgetProgram.setComputeUnitLimit({ units: 1400000 })
+                ])
+                .signers([payer])
+                .rpc({ skipPreflight: true });
+        });
+
+    } catch (e) {
+        console.error(e);
+    }
+});

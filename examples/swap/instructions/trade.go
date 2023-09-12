048eb0ee51844e79593179517fccd096a317f810
oxfeeefeee <pb01005051@gmail.com>
2023-09-13 00:41:12 +0800

    support inverse trade

--- examples/swap/instructions/trade.go
+++ examples/swap/instructions/trade.go
@@ -21,10 +21,12 @@ type IxTrade struct {
 
 	tokenProgram Program
 
-	// Trade amountA for at least expectedAmountB
-	amountA         uint64
-	expectedAmountB uint64
-	vaultAuthBump   uint8
+	// Trade offeredAmount A for at least expectedAmount B
+	offeredAmount  uint64
+	expectedAmount uint64
+	// If inverse is true, then the trade is offeredAmount B for at least expectedAmount A
+	inverse       bool
+	vaultAuthBump uint8
 }
 
 func (ix *IxTrade) Process() {
@@ -37,26 +39,38 @@ func (ix *IxTrade) Process() {
 	vaultB, err := token.UnpackAccount(ix.tokenBVault)
 	AbortOnError(err)
 	liquidity := math2.U64GeometryMean(vaultA.Amount, vaultB.Amount)
-	newVaultBAmount := math2.U64MulDiv(liquidity, liquidity, vaultA.Amount+ix.amountA)
-	amountB := vaultB.Amount - newVaultBAmount
-	if amountB < ix.expectedAmountB {
+
+	var leftToken, rightToken, leftVault, rightVault Account
+	var leftAmount, rightAmount uint64
+	if !ix.inverse {
+		leftToken, rightToken = ix.tokenA, ix.tokenB
+		leftVault, rightVault = ix.tokenAVault, ix.tokenBVault
+		leftAmount, rightAmount = vaultA.Amount, vaultB.Amount
+	} else {
+		leftToken, rightToken = ix.tokenB, ix.tokenA
+		leftVault, rightVault = ix.tokenBVault, ix.tokenAVault
+		leftAmount, rightAmount = vaultB.Amount, vaultA.Amount
+	}
+	newRightAmount := math2.U64MulDiv(liquidity, liquidity, leftAmount+ix.offeredAmount)
+	toTransfer := rightAmount - newRightAmount
+	if toTransfer < ix.expectedAmount {
 		panic("Cannot swap for the expected amount")
 	}
 
 	vaultAuthSeedBump := []SeedBump{{VAULT_AUTH_PDA_SEED, ix.vaultAuthBump}}
 	// Transfer token A to the pool
 	AbortOnError(token.Transfer(
-		ix.tokenA,
-		ix.tokenAVault,
+		leftToken,
+		leftVault,
 		ix.trader,
-		ix.amountA,
+		ix.offeredAmount,
 		nil,
 	))
 	AbortOnError(token.Transfer(
-		ix.tokenBVault,
-		ix.tokenB,
+		rightVault,
+		rightToken,
 		ix.vaultAuthority,
-		amountB,
+		toTransfer,
 		vaultAuthSeedBump,
 	))
 }
